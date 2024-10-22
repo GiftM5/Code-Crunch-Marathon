3947f6338c3632e0fa6ba142d0cec966590af101
GiftM5 <mofokeng_mpho@icloud.com>
2024-10-22 16:31:13 +0200

    updated

--- Rain_alert/main.py
+++ Rain_alert/main.py
@@ -1,11 +1,30 @@
+ 
 import requests,json
 from datetime import datetime
+import os
+from dotenv import load_dotenv
+
+
+
+load_dotenv()
+API_KEY = os.getenv("API_KEY")
+print(API_KEY)
+
 def get_data():
     main_url = "http://api.openweathermap.org/data/2.5/find?"
-    api_key = "a62b27850143efeed1afbd419aff0078"
+    api_key = API_KEY
     City = "Johannesburg"
     url = main_url + "appid=" + api_key +"&q=" + City
-    Data = requests.get(url).json()
-    print(Data)
+    Data = requests.get(url)
+    print(Data.json()['list'][0]['main']['temp'])
+    save_json(Data.json())
 
-get_data()
+
+
+def save_json(data):
+    with open("data.json","w") as f:
+        json.dump(data,f,indent=2)
+
+
+
+# get_data()
