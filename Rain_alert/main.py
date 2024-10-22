6e8332fefccccdea4e0290187dbb1654754069f5
GiftM5 <mofokeng_mpho@icloud.com>
2024-10-22 12:13:30 +0200

    updated

--- Rain_alert/main.py
+++ Rain_alert/main.py
@@ -1,9 +1,11 @@
-import requests
+import requests,json
+from datetime import datetime
+def get_data():
+    main_url = "http://api.openweathermap.org/data/2.5/find?"
+    api_key = "a62b27850143efeed1afbd419aff0078"
+    City = "Johannesburg"
+    url = main_url + "appid=" + api_key +"&q=" + City
+    Data = requests.get(url).json()
+    print(Data)
 
-main_url = "http://api.openweathermap.org/data/2.5/find?"
-api_key = "a62b27850143efeed1afbd419aff0078"
-City = "Johannesburg"
-url = main_url + "appid=" + api_key +"&q=" + City
-Data = requests.get(url).json()
-print(Data)
-
+get_data()
