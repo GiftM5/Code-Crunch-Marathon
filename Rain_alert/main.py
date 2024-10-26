8ab4c2c69672d2abcc08778ff54d254d98b5d057
Gift_M <mofokeng_mpho@icloud.com>
2024-10-26 16:18:43 +0200

    updated

--- Rain_alert/main.py
+++ Rain_alert/main.py
@@ -1,28 +1,50 @@
+from twilio.rest import Client
+import os
 from dotenv import load_dotenv
 import requests 
-from datetime import datetime
-import os
 
 
 
 
 load_dotenv()
 API_KEY = os.getenv("Api_key")
-print(API_KEY)
+    
+main_url = "http://api.openweathermap.org/data/2.5/find?"
+api_key = API_KEY
+City = "Johannesburg"
+url = main_url + "appid=" + api_key + "&q=" + City
 
-def get_data():
-    main_url = "http://api.openweathermap.org/data/2.5/find?"
-    api_key = API_KEY
-    City = "Johannesburg"
-    url = main_url + "appid=" + api_key + "&q=" + City
+account_sid = os.getenv("account_sid")
+auth_token = os.getenv("auth_token")
+    
+def rain_check():
     Data = requests.get(url)
-    print(Data.json()['list'][0]['main']['temp'])
-   
+    Data = Data.json()
+    
+    if Data.status_code == 200:
+        weather_conditions = Data["main"]
+        rain_expected = any("rain" in condition["main"].lower() for condition in weather_conditions)
+        
+        if rain_expected:
+            send_alert()
+        else:
+            send_alert()
+            print("NO RAIN EXPECTED!")
+    else:
+        print(Data.get("message")) 
 
-#still trying to commit 
+def send_alert():
+    
+    client = Client(account_sid, auth_token)
+    message = client.messages.create(
+    body = "Alert: Please be aware that rain is expected today!!,Do not forget your umbrella (-_-)",
+    from_='+12092850985',
+    to='+270652515201'
+    )
+    print("Alert_rain",message.sid)
 
 
 
 
-
-get_data()
+if __name__ == "main":
+    rain_check()
